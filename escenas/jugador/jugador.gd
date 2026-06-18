63b501eec7be0f4baa8365953543dc67973a9944
Fran <franmiceli08@gmail.com>
2026-06-18 00:33:53 -0300

    arrancamos terreno y mejoramos las animaciones del jugador al usar herramienta

--- escenas/jugador/jugador.gd
+++ escenas/jugador/jugador.gd
@@ -1,37 +1,40 @@
 extends CharacterBody2D
 
 @export var animacion: AnimatedSprite2D 
-@export var direccionVista: Marker2D
+@export var direccionVistaMarker: Marker2D
 
 var velocidad = 200.0
 var ultima_direccion = Vector2.DOWN
 var esta_actuando = false
-var posicionBase = "abajo"
+
+const DISTANCIA_CULTIVO = 32.0 # distancia a la que aparece el área de cultivo
 
 func _ready() -> void:
 	animacion.animation_finished.connect(_on_animation_finished)
+	_actualizar_area_cultivo(ultima_direccion)
 
 func _physics_process(_delta: float) -> void:
 	var direccionMovimiento = Input.get_vector("izquierda", "derecha", "arriba", "abajo")
 	velocity = velocidad * direccionMovimiento
 	
-	# agarramos la última dirección si se está moviendo
 	if direccionMovimiento != Vector2.ZERO:
 		ultima_direccion = direccionMovimiento
+		_actualizar_area_cultivo(ultima_direccion)
 
-	# si no usa la herramienta, mandamos la cosa
 	if Input.is_action_just_pressed("herramienta") and not esta_actuando:
 		esta_actuando = true
 		_procesar_animacion("herramienta", ultima_direccion)
 
-	# si no usa herramienta ahi hace correr o idle
 	if not esta_actuando:
 		if direccionMovimiento != Vector2.ZERO:
 			_procesar_animacion("correr", direccionMovimiento)
 		else:
 			_procesar_animacion("idle", ultima_direccion)
-
 	move_and_slide()
+
+func _actualizar_area_cultivo(direccion: Vector2) -> void:
+	# usamos el normalized para que cuando sea diagonal no se vaya a cualquier lado (aunque no se haga tanto diagonal...)
+	direccionVistaMarker.position = direccion.normalized() * DISTANCIA_CULTIVO
 
 func _procesar_animacion(estado: String, direccion: Vector2) -> void:
 	if abs(direccion.x) > abs(direccion.y):
