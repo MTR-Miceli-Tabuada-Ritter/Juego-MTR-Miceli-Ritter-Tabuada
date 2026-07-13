35335f683f97141345fab012f825b64b140b2b12
Fran <franmiceli08@gmail.com>
2026-07-12 23:55:27 -0300

    feat: reorden del audio

--- globales/sonidosGlobales.gd
+++ globales/sonidosGlobales.gd
@@ -7,28 +7,28 @@ var reproductorSonidoLogro: AudioStreamPlayer
 var reproductorSonidoSeleccionar: AudioStreamPlayer
 var reproductorSonidoPlantar: AudioStreamPlayer
 
-var sonidoSeleccionar1 = preload("res://Sonidos/Seleccionar1.mp3")
-var sonidoSeleccionar2 = preload("res://Sonidos/Seleccionar2.mp3")
+var sonidoSeleccionar1 = preload("res://recursos/audio/sonidos/Seleccionar1.mp3")
+var sonidoSeleccionar2 = preload("res://recursos/audio/sonidos/Seleccionar2.mp3")
 
 func _ready() -> void:
 	reproductorSonidoCaminar = AudioStreamPlayer.new()
 	add_child(reproductorSonidoCaminar)
-	reproductorSonidoCaminar.stream = preload("res://Sonidos/Caminar1.mp3")
+	reproductorSonidoCaminar.stream = preload("res://recursos/audio/sonidos/Caminar1.mp3")
 	reproductorSonidoCaminar.set_bus("SFX")
 
 	reproductorSonidoCortar = AudioStreamPlayer.new()
 	add_child(reproductorSonidoCortar)
-	reproductorSonidoCortar.stream = preload("res://Sonidos/Cortar1.mp3")
+	reproductorSonidoCortar.stream = preload("res://recursos/audio/sonidos/Cortar1.mp3")
 	reproductorSonidoCortar.set_bus("SFX")
 
 	reproductorSonidoRegar = AudioStreamPlayer.new()
 	add_child(reproductorSonidoRegar)
-	reproductorSonidoRegar.stream = preload("res://Sonidos/Regar.mp3")
+	reproductorSonidoRegar.stream = preload("res://recursos/audio/sonidos/Regar.mp3")
 	reproductorSonidoRegar.set_bus("SFX")
 
 	reproductorSonidoLogro = AudioStreamPlayer.new()
 	add_child(reproductorSonidoLogro)
-	reproductorSonidoLogro.stream = preload("res://Sonidos/Logro.mp3")
+	reproductorSonidoLogro.stream = preload("res://recursos/audio/sonidos/Logro.mp3")
 	reproductorSonidoLogro.set_bus("SFX")
 
 	reproductorSonidoSeleccionar = AudioStreamPlayer.new()
@@ -37,7 +37,7 @@ func _ready() -> void:
 
 	reproductorSonidoPlantar = AudioStreamPlayer.new()
 	add_child(reproductorSonidoPlantar)
-	reproductorSonidoPlantar.stream = preload("res://Sonidos/Plantar.mp3")
+	reproductorSonidoPlantar.stream = preload("res://recursos/audio/sonidos/Plantar.mp3")
 	reproductorSonidoPlantar.set_bus("SFX")
 
 func reproducirSonidoCaminar() -> void:
